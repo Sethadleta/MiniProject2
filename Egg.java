dcc3e2137c9157f56704833682ee7ec039851d2a
Sethadleta <adletash@mail.uc.edu>
2021-03-16 21:23:27 +0000

    Initial commit

---
+++ Egg.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+
+/**
+ * this class creates eggs as and assigns them a random color and contents
+ * @author Seth Adleta
+ * @version 3/16/21
+ */
+class Egg {
+  Random r = new Random();
+  private String eggColor;
+  private String eggContents;
+  String colorArray[] = {"blue", "pink", "yellow", "green" };
+  String contentArray[] = {"Cadbury Egg", "Reese's Egg", "pink Starburst", "yellow Peep", "25 cents", "50 cents", "one dollar" };
+
+  
+  /**
+   * This this is the constructor method for the egg class, it assigns a random
+   * egg color and a random egg contents
+   */
+  Egg() {
+    eggColor = randomColor();
+    eggContents = randomContents();
+  }
+  
+  /**
+   * This method generates a random number that it uses as an index to get a color
+   * value from the colorArray
+   * @return a color as a String
+   */
+  private String randomColor() {
+  int randomIndex = r.nextInt(4);
+  return colorArray[randomIndex];
+  }
+
+  /**
+   * This method generates a random number that it uses as an index to get egg
+   * contents from the contentArray
+   * @return the Egg contents as a String
+   */
+  private String randomContents() {
+  int randomIndex = r.nextInt(7);
+  return contentArray[randomIndex];
+  }
+
+  /**
+   * This is an accessor method for the egg color variable, it allows users to see
+   * the color of each egg
+   * @return egg color as string
+   */
+  String getColor() {
+    return eggColor;
+  }
+
+  /**
+   * This is an accessor method for the egg contents variable, it allows users to
+   * see the contents of each egg
+   * @return egg contents as string
+   */
+  String getContents() {
+    return eggContents;
+  }
+
+  /**
+   * this method prints out attributes about the egg. including the egg color and egg contents
+  */
+  void printEgg() {
+    System.out.println(getEggColor() + "egg contains " + getEggContents()+".");
+  }
+}
---
+++ Player.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+/**
+ * this class keeps track of each player and their basket
+ * @author Seth Adleta
+ * @version 3/16/21
+ */
+class Player {
+  private int numEggs;
+  private ArrayList<Egg> basket;
+
+  /**
+   * this is the constructor method for the player class
+   */
+  Player() {
+    numEggs = 0;
+    basket = new ArrayList<Egg>();
+  }
+
+  /**
+   * this is the acessor method for the numEggs varible.
+   * @return the number of eggs in a players basket as an int
+   */
+  int getNumEggs() {
+    return numEggs;
+  }
+
+  /**
+   * this is the acessor method for the basket arraylist. it gives the eggs in the
+   * player's basket
+   * @return the player's basket as an ArrayList
+   */
+  ArrayList<Egg> getBasket() {
+    return basket;
+  }
+
+  /**
+   * this method prints out each egg in the basket
+   */
+  void printBasket() {
+    for (Egg i : basket) {
+      i.printEgg();
+    }
+  }
+
+  /**
+   * this method adds an egg to the player's basket and then adds 1 to the number of eggs
+   * @param the method asks for an Egg object to be passed through
+  */
+  void foundEgg(Egg egg) {
+    numEggs ++;
+    basket.add(egg);
+  }
+  
+}
